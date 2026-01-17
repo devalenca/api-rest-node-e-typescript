ee899d70c602617374b97b9bd4771bb5caca2a7a
devalenca <alencargabriel1207@gmail.com>
2026-01-16 22:30:56 -0300

    feat: added yup translations and aborty early on validate method

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -5,25 +5,31 @@ import * as yup from "yup";
 
 interface ICidade{
   nome: string;
+  estado: string;
 }
 
 const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
-  nome: yup.string().required("Nome is required").min(3, "Nome must be at least 3 characters long"),
+  nome: yup.string().required().min(3),
+  estado: yup.string().required().min(3),
 });
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
   let validatedData: ICidade | undefined = undefined;
+
   const data: ICidade = req.body;
 
   try {
-    validatedData = await bodyValidation.validate(req.body);
-  } catch (error) {
-    const yupError = error as yup.ValidationError;
-    return res.status(StatusCodes.BAD_REQUEST).json({ error:
-      {
-        default: yupError.message,
-      }
-    });
+    validatedData = await bodyValidation.validate(req.body, { abortEarly: false });
+  } catch (err) {
+    const yupError = err as yup.ValidationError;
+    const errors: Record<string, string> = {};
+
+    yupError.inner.forEach(error => {
+      if (error.path === undefined) return;
+      errors[error.path] = error.message;
+    })
+
+    return res.status(StatusCodes.BAD_REQUEST).json({ error: errors });
   }
 
   console.log("Creating cidade with data:", data);
